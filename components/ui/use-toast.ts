a1a64212fe754db48b170456240d71f89a5b4523
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-01-27 19:53:46 +0800

    feat: initialize convex db

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -18,6 +18,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
+//typescript-eslint/no-unused-vars
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
