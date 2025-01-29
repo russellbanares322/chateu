10e066c33585ba6875afc572f11c1f7d2c0c15c2
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-01-29 21:06:50 +0800

    feat: put SignInWithGoogle sign in card component to a separate file

--- app/(splash)/GetStarted/GetStarted.tsx
+++ app/(splash)/GetStarted/GetStarted.tsx
@@ -4,11 +4,8 @@ import { SignInWithGoogle } from "./SignInWithGoogle";
 
 export const GetStarted = () => {
   return (
-    <div className="flex grow flex-col">
-      <h1 className="flex items-center justify-center">
-        Getting Started Page
-        {<SignInWithGoogle />}
-      </h1>
+    <div className="flex items-center justify-center min-h-screen w-full max-w-[500px] mx-auto">
+      <SignInWithGoogle />
     </div>
   );
 };
--- app/(splash)/GetStarted/SignInWithGoogle.tsx
+++ app/(splash)/GetStarted/SignInWithGoogle.tsx
@@ -1,17 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { useAuthActions } from "@convex-dev/auth/react";
+import Image from "next/image";
+import googleIcon from "@/components/images/google.png";
 
 export function SignInWithGoogle() {
   const { signIn } = useAuthActions();
   return (
-    <Button
-      className="flex-1"
-      variant="outline"
-      type="button"
-      // onClick={() => void signIn("github", { redirectTo: "/product" })}
-      onClick={() => void signIn("google")}
-    >
-      Google
-    </Button>
+    <div className="shadow-md border rounded-xl p-3 w-full space-y-2">
+      <h1 className="text-2xl text-center mb-5">Welcome to CHATEU</h1>
+      <Button
+        className="w-full"
+        variant="outline"
+        type="button"
+        // onClick={() => void signIn("github", { redirectTo: "/product" })}
+        onClick={() => void signIn("google")}
+      >
+        <Image
+          height={20}
+          width={20}
+          alt="Sign In with Google"
+          src={googleIcon}
+        />
+        Sign In with Google
+      </Button>
+    </div>
   );
 }
