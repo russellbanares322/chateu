fff60f275162b6b694638c5e9941a793b6957ec4
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-02-13 21:12:51 +0800

    feat: map custom user in sidebar component

--- app/chats/_layout/sidebar.tsx
+++ app/chats/_layout/sidebar.tsx
@@ -1,7 +1,19 @@
 export default function Sidebar() {
+  const sampleData = new Array(10).fill("").map((_, index) => ({
+    id: index,
+    name: `User ${index + 1}`,
+  }));
+
   return (
     <div className="p-10">
-      <h1 className="text-white">Sidebar</h1>
+      <h1 className="text-white mb-5">CHATEU</h1>
+      <div className="space-y-5">
+        {sampleData?.map((user) => (
+          <div key={user.id}>
+            <p className="text-white">{user.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
