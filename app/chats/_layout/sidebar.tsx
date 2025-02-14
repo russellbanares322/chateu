4294387dc3c8b93cbef529f59c291aaf478edb92
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-02-14 20:20:06 +0800

    chore: add comment in sidebar component

--- app/chats/_layout/sidebar.tsx
+++ app/chats/_layout/sidebar.tsx
@@ -4,6 +4,7 @@ export default function Sidebar() {
     name: `User ${index + 1}`,
   }));
 
+  // Map real data
   return (
     <div className="p-10">
       <h1 className="text-white mb-5">CHATEU</h1>
