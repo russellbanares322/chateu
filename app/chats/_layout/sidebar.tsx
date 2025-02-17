d13af556b0d1bb77bd6a9f92db76d9744efa5e2b
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-02-17 21:33:54 +0800

    chore: convert chatId value to string in sidebar component

--- app/chats/_layout/sidebar.tsx
+++ app/chats/_layout/sidebar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Sidebar() {
   const sampleData = new Array(10).fill("").map((_, index) => ({
-    chatId: index + 1,
+    chatId: String(index + 1),
     id: index,
     name: `User ${index + 1}`,
   }));
@@ -13,7 +13,7 @@ export default function Sidebar() {
       <h1 className="text-white mb-5 ml-1">CHATEU</h1>
       <ul className="space-y-3 w-full">
         {sampleData?.map((user) => (
-          <Link key={user.id} href={`chats/${user.chatId}`}>
+          <Link key={user.id} href={`/chats/${user.chatId}`}>
             <li className="text-white cursor-pointer hover:bg-slate-400 py-1 px-2 rounded-md">
               {user.name}
             </li>
