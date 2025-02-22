5cedd9f41b682ce0b6f8b710fe00264d5734dc31
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-02-22 12:22:10 +0800

    feat: dynamically display emoji picker

--- app/chats/[chatId]/_layout/chat-input.tsx
+++ app/chats/[chatId]/_layout/chat-input.tsx
@@ -1,8 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { SendIcon } from "lucide-react";
-import React from "react";
+import { SendIcon, SmileIcon } from "lucide-react";
+import EmojiPicker from "emoji-picker-react";
+import { useState } from "react";
 
 export default function ChatInput() {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
+  const onToggleShowEmojiPicker = () => {
+    setShowEmojiPicker((prev) => !prev);
+  };
+
   return (
     <div className="flex items-center bg-gray-100 gap-1 absolute rounded-md bottom-0 left-0 right-0 mx-auto w-[950px] mb-4 px-2">
       <textarea
@@ -10,9 +18,23 @@ export default function ChatInput() {
         className="border max-h-[900px] rounded-md p-1 bg-gray-100  w-full outline-none border-none resize-none"
         placeholder="Enter your chat here..."
       />
-      <Button>
-        Send <SendIcon />
-      </Button>
+      <div className="flex items-center gap-3">
+        <div className="relative">
+          <SmileIcon
+            onClick={onToggleShowEmojiPicker}
+            className="cursor-pointer"
+            size={20}
+          />
+          {showEmojiPicker && (
+            <div className="absolute bottom-6 right-3">
+              <EmojiPicker />
+            </div>
+          )}
+        </div>
+        <Button>
+          Send <SendIcon />
+        </Button>
+      </div>
     </div>
   );
 }
