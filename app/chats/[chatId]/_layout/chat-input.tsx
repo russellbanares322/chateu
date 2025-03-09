7d3ac91b673aa38e39a454db3829cd2e800ae63c
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-03-09 21:45:23 +0800

    feat: instantiate onSubmit function in chat input page

--- app/chats/[chatId]/_layout/chat-input.tsx
+++ app/chats/[chatId]/_layout/chat-input.tsx
@@ -12,8 +12,15 @@ export default function ChatInput() {
     setShowEmojiPicker((prev) => !prev);
   };
 
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="flex items-center bg-gray-100 gap-1 absolute rounded-md bottom-0 left-0 right-0 mx-auto w-[950px] mb-4 px-2">
+    <form
+      onSubmit={onSubmit}
+      className="flex items-center bg-gray-100 gap-1 absolute rounded-md bottom-0 left-0 right-0 mx-auto w-[950px] mb-4 px-2"
+    >
       <textarea
         autoFocus
         className="border max-h-[900px] rounded-md p-1 bg-gray-100  w-full outline-none border-none resize-none"
