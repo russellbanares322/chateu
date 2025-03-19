fe39a9e447ae1fbc3a7fbf1f170d087b8b838692
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-03-19 19:57:58 +0800

    style: hide sidedbar in small screen device

--- app/chats/_layout/chat-layout.tsx
+++ app/chats/_layout/chat-layout.tsx
@@ -13,11 +13,11 @@ export default function ChatLayout({
       {hasChats && (
         <div className="bg-black h-full grid grid-cols-8 min-h-screen">
           {/* Sidebar */}
-          <div className="col-span-1 w-full h-full">
+          <div className="md:col-span-1 w-full h-full hidden md:block">
             <Sidebar />
           </div>
           {/* Main Content */}
-          <div className="col-span-7 w-full max-h-[900px] h-full">
+          <div className="col-span-8 md:col-span-7 w-full max-h-[900px] h-full">
             <div className="bg-white p-10 m-7 rounded-xl h-full">
               {children}
             </div>
