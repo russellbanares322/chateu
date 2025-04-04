a7904f6d6b96ab32fa9f0ce7ce075086c82161b4
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-04-04 19:53:17 +0800

    feat: instantiate block user alert dialog

--- app/chats/[chatId]/_layout/chat-display.tsx
+++ app/chats/[chatId]/_layout/chat-display.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -11,6 +21,7 @@ import {
 import { DropdownItem } from "@/constants/types";
 import { cn } from "@/lib/utils";
 import { Bell, EllipsisIcon, UserIcon, UserX } from "lucide-react";
+import { useState } from "react";
 
 type ChatDisplayProps = {
   chatId: string;
@@ -26,6 +37,8 @@ const dummyChatData = Array.from({ length: 15 })
 
 export default function ChatDisplay({ chatId }: ChatDisplayProps) {
   const userId = "XXY";
+  const [showBlockUserAlert, setShowBlockUserAlert] = useState(false);
+
   // Implement showing of alert before blocking a user
   const userDropdownItems: DropdownItem[] = [
     {
@@ -48,7 +61,7 @@ export default function ChatDisplay({ chatId }: ChatDisplayProps) {
       key: 4,
       label: "Block",
       icon: <UserX />,
-      action: () => console.log("Block"),
+      action: () => setShowBlockUserAlert(true),
     },
   ];
 
@@ -74,7 +87,7 @@ export default function ChatDisplay({ chatId }: ChatDisplayProps) {
               return (
                 <div key={item.key}>
                   {!item.divider && (
-                    <DropdownMenuItem>
+                    <DropdownMenuItem onClick={item.action}>
                       {item.icon} {item.label}
                     </DropdownMenuItem>
                   )}
@@ -115,6 +128,25 @@ export default function ChatDisplay({ chatId }: ChatDisplayProps) {
           );
         })}
       </div>
+      {/* Block User Alert Confirmation Dialog */}
+      <AlertDialog
+        open={showBlockUserAlert}
+        onOpenChange={setShowBlockUserAlert}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
