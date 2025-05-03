0cf1215a946e9daaa436bf856792e30486df438c
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-05-03 13:09:29 +0800

    chore: add comment in profile page

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -7,6 +7,7 @@ type ProfileProps = {
 export default async function Profile({ params }: ProfileProps) {
   const { userId } = await params;
 
+  // Display image of user and details
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="bg-white p-3 rounded-md">User Profile {userId}</div>
