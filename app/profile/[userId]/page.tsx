a50dcc1214c82b45771cc43545504e89bd58b9aa
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-05-04 20:53:26 +0800

    feat: add extra buttons in user profile card

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Link2 } from "lucide-react";
+
 type ProfileProps = {
   params: Promise<{
     userId: string;
@@ -12,10 +15,22 @@ export default async function Profile({ params }: ProfileProps) {
       <div className="bg-white rounded-xl max-w-[500px] w-full mx-5 relative">
         <div className="bg-gray/15 w-full h-16 rounded-tl-xl rounded-tr-xl absolute" />
         <div className="p-5 mt-4">
-          {/* Image and name of user */}
-          <div className="space-y-2">
-            <div className="bg-gray h-16 w-16 rounded-full border-[3px] border-solid border-white" />
-            <p className="text-lg font-semibold">FullName - {userId}</p>
+          {/* Image and name of user*/}
+          <div className="flex justify-between items-center">
+            <div className="space-y-2">
+              <div className="bg-gray h-16 w-16 rounded-full border-[3px] border-solid border-white" />
+              <p className="text-lg font-semibold">FullName - {userId}</p>
+            </div>
+            {/* Extra action buttons */}
+            <div className="mt-1">
+              <Button
+                size="sm"
+                className="border border-solid bg-white hover:bg-gray/10 text-black font-semibold"
+              >
+                <Link2 className="text-black" />
+                Copy link
+              </Button>
+            </div>
           </div>
         </div>
       </div>
