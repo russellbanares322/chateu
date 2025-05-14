668db9b189433ff53e47fd7cb6ef0369b12f9556
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-05-14 21:17:34 +0800

    style: set bg color on select sidebar

--- app/chats/_layout/sidebar.tsx
+++ app/chats/_layout/sidebar.tsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+  const pathname = usePathname();
   const sampleData = new Array(10).fill("").map((_, index) => ({
     chatId: String(index + 1),
     id: index,
@@ -11,10 +16,17 @@ export default function Sidebar() {
   return (
     <div className="py-14 pl-2">
       <h1 className="text-white mb-5 ml-1">CHATEU</h1>
-      <ul className="space-y-3 w-full">
+      <ul className="space-y-10  w-full">
         {sampleData?.map((user) => (
           <Link key={user.id} href={`/chats/${user.chatId}`}>
-            <li className="text-white cursor-pointer hover:bg-slate-400 py-1 px-2 rounded-md">
+            <li
+              className={cn(
+                "text-white cursor-pointer mb-1 py-1 px-2 rounded-md",
+                pathname.includes(String(user.id))
+                  ? "bg-blue-500"
+                  : "bg-none hover:bg-slate-300 hover:text-black",
+              )}
+            >
               {user.name}
             </li>
           </Link>
