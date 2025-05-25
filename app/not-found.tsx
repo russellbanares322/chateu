d117fae7da4b980acfc5c09d245f8fe10c28007c
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-05-25 21:09:06 +0800

    style: make not found page responsive

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,17 +6,21 @@ import { ArrowLeftIcon } from "lucide-react";
 
 export default function NotFound() {
   return (
-    <div className="flex items-center flex-wrap justify-center gap-1 md:gap-40 min-h-screen w-full">
+    <div className="flex items-center flex-wrap-reverse justify-center gap-1 md:gap-40 md:min-h-screen w-full">
       <div className="space-y-5">
         <p className="text-7xl font-semibold">Oops!</p>
-        <p className="max-w-[250px] text-xl text-gray-500">
+        <p className="md:max-w-[250px] text-xl text-gray-500">
           We couldn't find the page you're looking for.
         </p>
         <Button>
           <ArrowLeftIcon /> Go Home
         </Button>
       </div>
-      <Image alt="Page not found" src={notFound} height={400} width={400} />
+      <Image
+        className="h-[300px] w-[300px] md:h-[400px] md:w-[400px]"
+        alt="Page not found"
+        src={notFound}
+      />
     </div>
   );
 }
