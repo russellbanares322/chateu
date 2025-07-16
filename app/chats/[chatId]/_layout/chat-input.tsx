0599fc20c8cea611987e5f79b214e93ee9f722fa
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-07-16 20:38:52 +0800

    feat: log event of onSubmit function in chat-input component

--- app/chats/[chatId]/_layout/chat-input.tsx
+++ app/chats/[chatId]/_layout/chat-input.tsx
@@ -17,6 +17,7 @@ export default function ChatInput() {
     const formData = new FormData(e.currentTarget);
     const message = formData.get("message");
     alert(message);
+    console.log(e);
   };
 
   return (
