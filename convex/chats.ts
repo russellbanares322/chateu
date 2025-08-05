d4fc09597fbc16fd888902101ecb98be0f11c69b
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-08-05 22:58:38 +0800

    chore: remove comment in  chats convex

--- convex/chats.ts
+++ convex/chats.ts
@@ -1,12 +1,11 @@
 import { v } from "convex/values";
 import { query } from "./_generated/server";
 
-// Instantiate post and update api call
 export const chats = query({
     args: { chatSessionId: v.string() },
     handler: async (ctx, args) => {
         const chatDatas = await ctx.db.query("chats").filter((q) => q.eq(q.field("chatSessionId"), args.chatSessionId)).order("desc")
-      
+
         return chatDatas
     }
 })
