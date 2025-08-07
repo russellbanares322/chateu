baa0eda6dc667e9ca22740adf2116fd4def4ba8b
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-08-07 19:48:18 +0800

    feat: only put google and convex auth as authentication

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,9 +1,7 @@
-import GitHub from "@auth/core/providers/github";
-import Resend from "@auth/core/providers/resend";
 import Google from "@auth/core/providers/google";
 
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [GitHub, Resend, Google],
+  providers: [Google],
 });
