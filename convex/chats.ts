79fd0033cf1cd86c3b3b2cf4e4e0258c3fff50e7
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-09-02 20:08:14 +0800

    chore: log context in chats query

--- convex/chats.ts
+++ convex/chats.ts
@@ -4,6 +4,7 @@ import { query } from "./_generated/server";
 export const chats = query({
     args: { chatSessionId: v.string() },
     handler: async (ctx, args) => {
+        console.log(ctx)
         const chatDatas = await ctx.db.query("chats").filter((q) => q.eq(q.field("chatSessionId"), args.chatSessionId)).order("desc")
 
         return chatDatas
