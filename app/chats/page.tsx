071a2eadbfd6efadbcd6997d8c2449d996aef007
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-09-11 20:01:23 +0800

    chore: change chats page title

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -4,7 +4,7 @@ export default function ChatPage() {
   return (
     <ChatLayout>
       <div className="flex items-center justify-center w-full h-full">
-        <h1>Start connecting with other people</h1>
+        <h1>Start connecting with people</h1>
       </div>
     </ChatLayout>
   );
